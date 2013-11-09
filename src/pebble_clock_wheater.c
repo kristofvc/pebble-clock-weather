70a5e93d8347cf5a6f4e0fccf9254904f0e11e9f
Kristof Van Cauwenbergh <kristof@king-foo.be>
2013-11-09 13:17:30 +0100

    datetime view

--- src/pebble_clock_wheater.c
+++ src/pebble_clock_wheater.c
@@ -1,60 +1,82 @@
-#include <pebble.h>
+#include "pebble.h"
 
-static Window *window;
-static TextLayer *text_layer;
+Window *window;
+TextLayer *text_date_layer;
+TextLayer *text_time_layer;
+Layer *line_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+void line_layer_update_callback(Layer *layer, GContext* ctx) {
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  static char time_text[] = "00:00";
+  static char date_text[] = "Xxxxxxxxx 00";
+
+  char *time_format;
+
+  strftime(date_text, sizeof(date_text), "%B %e", tick_time);
+  text_layer_set_text(text_date_layer, date_text);
+
+  if (clock_is_24h_style()) {
+    time_format = "%R";
+  } else {
+    time_format = "%I:%M";
+  }
+
+  strftime(time_text, sizeof(time_text), time_format, tick_time);
+
+  // Kludge to handle lack of non-padded hour format string
+  // for twelve hour clock.
+  if (!clock_is_24h_style() && (time_text[0] == '0')) {
+    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+  }
+
+  text_layer_set_text(text_time_layer, time_text);
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
-
-static void window_load(Window *window) {
+void draw_watchface() {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  text_date_layer = text_layer_create((GRect) { .origin = { 10, 7 }, .size = { bounds.size.w, 25 } });
+  text_layer_set_text_color(text_date_layer, GColorWhite);
+  text_layer_set_background_color(text_date_layer, GColorClear);
+  text_layer_set_font(text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
+  layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
+
+  text_time_layer = text_layer_create((GRect) { .origin = { 7, 20 }, .size = { bounds.size.w, 50 } });
+  text_layer_set_text_color(text_time_layer, GColorWhite);
+  text_layer_set_background_color(text_time_layer, GColorClear);
+  text_layer_set_font(text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
+  layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
+
+  GRect line_frame = (GRect) { .origin = { 0, bounds.size.h - 2 - bounds.size.h/2 }, .size = { bounds.size.w, 4 } };
+  line_layer = layer_create(line_frame);
+  layer_set_update_proc(line_layer, line_layer_update_callback);
+  layer_add_child(window_layer, line_layer);
 }
 
-static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
-}
-
-static void init(void) {
+void init(void) {
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+  window_stack_push(window, true /* Animated */);
+  window_set_background_color(window, GColorBlack);
+  
+  draw_watchface();
+
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 }
 
-static void deinit(void) {
-  window_destroy(window);
+void deinit(void) {
+  tick_timer_service_unsubscribe();
 }
 
 int main(void) {
   init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed watchface clock-wheater: %p", window);
   app_event_loop();
+  
   deinit();
 }
