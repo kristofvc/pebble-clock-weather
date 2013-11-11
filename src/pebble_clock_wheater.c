242df0b9c2c5be234d62aac9a4767930db4738a6
Kristof Van Cauwenbergh <kristof@king-foo.be>
2013-11-11 11:27:16 +0100

    wheather view (no functionality yet)

--- src/pebble_clock_wheater.c
+++ src/pebble_clock_wheater.c
@@ -5,6 +5,48 @@ TextLayer *text_date_layer;
 TextLayer *text_time_layer;
 Layer *line_layer;
 
+static TextLayer *temperature_layer;
+static char temperature[16];
+
+static BitmapLayer *icon_layer;
+static GBitmap *icon_bitmap = NULL;
+
+static AppSync sync;
+static uint8_t sync_buffer[32];
+
+enum WeatherKey {
+  WEATHER_ICON_KEY = 0x0,         // TUPLE_INT
+  WEATHER_TEMPERATURE_KEY = 0x1,  // TUPLE_CSTRING
+};
+
+static uint32_t WEATHER_ICONS[] = {
+    RESOURCE_ID_IMAGE_SUN,
+    RESOURCE_ID_IMAGE_CLOUD,
+    RESOURCE_ID_IMAGE_RAIN,
+    RESOURCE_ID_IMAGE_SNOW
+};
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
+  switch (key) {
+    case WEATHER_ICON_KEY:
+      if (icon_bitmap) {
+        gbitmap_destroy(icon_bitmap);
+      }
+
+      icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[new_tuple->value->uint8]);
+      bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
+      break;
+
+    case WEATHER_TEMPERATURE_KEY:
+      // App Sync keeps new_tuple in sync_buffer, so we may use it directly
+      text_layer_set_text(temperature_layer, new_tuple->value->cstring);
+      break;
+  }
+}
+
 void line_layer_update_callback(Layer *layer, GContext* ctx) {
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
@@ -52,10 +94,28 @@ void draw_watchface() {
   text_layer_set_font(text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
   layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
 
-  GRect line_frame = (GRect) { .origin = { 0, bounds.size.h - 2 - bounds.size.h/2 }, .size = { bounds.size.w, 4 } };
+  GRect line_frame = (GRect) { .origin = { 0, (bounds.size.h/2) - 2 }, .size = { bounds.size.w, 4 } };
   line_layer = layer_create(line_frame);
   layer_set_update_proc(line_layer, line_layer_update_callback);
   layer_add_child(window_layer, line_layer);
+
+  icon_layer = bitmap_layer_create(GRect(7, 90, 80, 80));
+  layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
+
+  temperature_layer = text_layer_create((GRect) { .origin = { (bounds.size.w / 2) + 20, (bounds.size.h/2) + 9 }, .size = { bounds.size.w, 50 } });
+  text_layer_set_text_color(temperature_layer, GColorWhite);
+  text_layer_set_background_color(temperature_layer, GColorClear);
+  text_layer_set_font(temperature_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
+  text_layer_set_text(temperature_layer, temperature);
+
+  Tuplet initial_values[] = {
+    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
+    TupletCString(WEATHER_TEMPERATURE_KEY, "99\u00B0C"),
+  };
+  app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
+      sync_tuple_changed_callback, sync_error_callback, NULL);
+
+  layer_add_child(window_layer, text_layer_get_layer(temperature_layer));
 }
 
 void init(void) {
